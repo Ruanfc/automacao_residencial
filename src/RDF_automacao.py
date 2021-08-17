e3ff073560bdbf37543fb08f54c84738a7c6a136
Ruan Flaneto Cartier <ruan.cartier@ufpe.br>
2021-08-16 23:48:06 -0300

    Frontend Added
    Still There are some problems that I even don't know why, such as the
    last button.

---
+++ src/RDF_automacao.py
@@ -0,0 +1,69 @@
+# projeto de automação residencial
+# autor Flávio Cândido, Ruan Flaneto, Diego Rosas
+# v1.0
+
+from PyQt5 import uic, QtWidgets
+from grafico_figura import *
+from time import sleep
+import socket
+
+
+def liga_lampada(id):
+    eval("tela.frame_desligado" + index[id]).close()
+    eval("tela.frame_ligado" + index[id]).show()
+
+
+def desliga_lampada(id):
+    eval("tela.frame_desligado" + index[id]).show()
+    eval("tela.frame_ligado" + index[id]).close()
+
+
+def lampadas():
+    tela.luz_frame.show()
+    tela.grafico_frame.close()
+
+
+def grafico():
+    tela.luz_frame.close()
+    gera_graf()
+    # sleep()
+    # tela.grafico_frame=MatplotlibWidget()
+    tela.grafico_frame.show()
+    # tela.grafWid.show()
+
+
+index = ["", "_2", "_7", "_8", "_9"]
+n = len(index)
+ipIndex = []
+s = []
+for i, id in enumerate(index):
+    ipIndex.append("192.168.1.18" + str(i))
+    s.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+    s[-1].settimeout(10)
+    # Precisa ter cuidado ao não conectar, talvez um timeout sirva
+    s[-1].connect((ipIndex[-1], 8266))
+    pass
+
+
+aplicacao = QtWidgets.QApplication([])
+tela = uic.loadUi("untitled.ui")
+tela.btn_grafico.clicked.connect(grafico)
+tela.btn_luzes.clicked.connect(lampadas)
+for i in range(n):
+    eval(
+        "tela.btn_liga"
+        + index[i]
+        + ".clicked.connect(lambda i : liga_lampada("
+        + str(i)
+        + "))"
+    )
+    eval(
+        "tela.btn_desliga"
+        + index[i]
+        + ".clicked.connect(lambda i : desliga_lampada("
+        + str(i)
+        + "))"
+    )
+
+tela.show()
+aplicacao.exec()
---
+++ src/grafico_figura.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt  
+y = []
+x = []
+
+def gera_graf():
+    with open('dados.txt', 'r') as f:
+    # f= open(, 'r')
+        dados = f.read()
+        for linha in dados.split('\n'):
+            if len(linha) == 0:
+                continue
+            xi, yi = linha.split(',')
+            x.append(float(xi))
+            y.append(float(yi))
+        # y = []
+        # x = []#x = ['Lâmpada 1', 'Lâmpada 2', 'Lâmpada 3', 'lâmpada 4']
+        # y = [2, 5, 7, 10]
+
+    figura = plt.figure(figsize=(10,6))
+    figura.suptitle('Gráfico de acionamento das lâmpadas')
+
+    figura.add_subplot(111)
+    plt.bar(x, y, color='g',)
+    plt.ylabel('Quantidade de horas')
+    plt.xlabel('Lâmpadas')
+    plt.yticks([0,1,2,3,4,5,6,7,8,9,10,11,12])
+    #plt.axis(ymin=0,ymax=12)
+
+
+    plt.savefig('Figure_1.png', dpi =100)
+    # plt.savefig('gaficos img\\Figure_1.png', dpi =100)
+    # plt.show()
+    
+
